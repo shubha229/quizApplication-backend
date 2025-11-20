bebb753dd5bb87067edcaadddbe00bfdac4ca90f
shubha229 <shubhashreenayak9825@gmail.com>
2025-11-20 23:00:05 +0530

    Update Score.js

---
+++ models/Score.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const ScoreSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  domain: { type: String, required: true },
+  score: { type: Number, required: true },
+  total: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Score", ScoreSchema);
